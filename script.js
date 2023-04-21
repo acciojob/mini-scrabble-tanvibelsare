4c19251d503479c17b0bd4ae458bc8aacfd983c2
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-04-21 08:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let input=document.getElementById("evaluatedText")
+let heading=document.getElementById("letterCount")
+input.addEventListener("keyup",()=>{
+	if(input.value==null){
+		heading.innerText=input.value.length;
+	}
+})
